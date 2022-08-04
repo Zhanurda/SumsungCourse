ca40e237233037628cfa77bd53e43a65a654537b
Администратор <Zhanurda>
2022-08-04 16:10:36 +0600

    added users

--- Files/users.py
+++ Files/users.py
@@ -1,7 +1,7 @@
 class User:
     def __init__(self,name):
-        self.name = name
+        self.surname = surname
 
 
     def __str__(self):
-        return self.name
+        return self.surname
